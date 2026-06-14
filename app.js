4ecea8ad6d60cbfca2aa7634ace2a8ebbe3b7d7a
dragokas371158989-png <dragokas371158989@gmail.com>
2026-06-14 12:28:34 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const GKM_APP_CLEAN_VERSION = "v56-clean-builder-data-fix-2026-06-13";
+const GKM_APP_CLEAN_VERSION = "v57-force-postfix-build-2026-06-13";
 
 const FAST_BASE = "data/fast";
 const FAST_HOME_URL = `${FAST_BASE}/home.json`;
@@ -4429,3 +4429,6 @@ window.GKM_STRICT_VISIBLE_TITLE_SEARCH_VERSION = "v50-strict-visible-title-searc
 
 
 window.GKM_BUILD_DATA_FIX_VERSION = "v56-clean-builder-data-fix-2026-06-13";
+
+
+window.GKM_FORCE_POSTFIX_BUILD_VERSION = "v57-force-postfix-build-2026-06-13";
